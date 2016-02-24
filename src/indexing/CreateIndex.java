f92e36ed2473b9e1d3ce94392186d0f4e82374c0
AnkurPande <ankur.pandey111@gmail.com>
2016-02-24 01:52:09 -0500

    Initial commit for timer and memory functions.

--- src/indexing/CreateIndex.java
+++ src/indexing/CreateIndex.java
@@ -5,11 +5,40 @@
 
 package indexing;
 
+import input_output.IOFile;
+
+import java.io.IOException;
+
 public class CreateIndex {
-
-	public static void main(String[] args) {
+	
+	static String path = "C:\\Users\\Ankurp\\DENSE-INDEX\\";
+	static String FILE_NAME = path+"person.txt";
+	static int  NO_OF_TOUPLES = 10000;
+	
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		new CreateIndex().runCases();
 	}
 
+	private void runCases() throws IOException{
+		String[] tempStringArr = new String[40];
+		IOFile f = new IOFile(FILE_NAME);
+		//Calculate available memory at start.
+		float startmem = Runtime.getRuntime().freeMemory();
+		
+		//start time
+		long startTime = System.currentTimeMillis();	
+		
+		int runs = (int) Math.ceil((double)NO_OF_TOUPLES/(40));
+		
+		
+		long endTime = System.currentTimeMillis();
+		
+		tempStringArr = null;
+		long endmem = Runtime.getRuntime().totalMemory();
+		System.out.println("Time Taken : " + (endTime - startTime) + "ms");
+		System.out.println("Memory Taken (in bytes): " + (endmem - startmem) + " bytes");
+		System.out.println("Memory Taken (in MB): " + (double) (endmem - startmem) /(1024*1024) + " MB");
+		
+		}	
 }
