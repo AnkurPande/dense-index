d70e91f35461ca1f7b9816e820bf86b9b2f862a5
Richard Kallos <rkallos@gmail.com>
2016-02-26 16:49:05 -0500

    Changed IOFile to use FileChannel and ByteBuffers to read blocks.

--- src/input_output/IOFile.java
+++ src/input_output/IOFile.java
@@ -5,55 +5,55 @@
 
 package input_output;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class IOFile {
 	public static final int BLOCK_SIZE = 4096;
+	
+	private static int reads = 0;
+	private static int writes = 0;
 
 	FileInputStream fInStream;
-	DataInputStream in;
-	BufferedReader br;
+	FileChannel fc;
 
 	public IOFile(String filename) throws FileNotFoundException {
 		fInStream = new FileInputStream(filename);
-		in = new DataInputStream(fInStream);
-		br = new BufferedReader(new InputStreamReader(in));
+		fc = fInStream.getChannel();
 	}
 
-	public String readLineFromFile(String filename) {
-		try {
-			String strline;
-			while ((strline = br.readLine()) != null) {
-				return strline;
-			}
-			in.close();
-		} catch (Exception e) {
-			System.out.println("ERROR :" + e.getMessage());
-		}
-		return "";
+//	public String readLineFromFile(String filename) {
+//		try {
+//			String strline;
+//			while ((strline = br.readLine()) != null) {
+//				return strline;
+//			}
+//			in.close();
+//		} catch (Exception e) {
+//			System.out.println("ERROR :" + e.getMessage());
+//		}
+//		return "";
+//
+//	}
 
-	}
-
-	public String readTupleFromFile(String filename) throws IOException {
-		char[] chars = new char[100];
-		int offset = 0;
-		while (offset < 100) {
-			int charsRead = br.read(chars, offset, 100 - offset);
-			if (charsRead <= 0) {
-				throw new IOException("Stream terminated early");
-			}
-			offset += charsRead;
-		}
-		return new String(chars);
-
-	}
+//	public String readTupleFromFile(String filename) throws IOException {
+//		char[] chars = new char[100];
+//		int offset = 0;
+//		while (offset < 100) {
+//			int charsRead = fc.read(chars, offset, 100 - offset);
+//			if (charsRead <= 0) {
+//				throw new IOException("Stream terminated early");
+//			}
+//			offset += charsRead;
+//		}
+//		return new String(chars);
+//
+//	}
 
 	public void writeToFile(String filename, String[] linesToWrite)
 			throws FileNotFoundException, UnsupportedEncodingException {
@@ -67,9 +67,24 @@ public class IOFile {
 	/**
 	 * @author Julian
 	 */
-	public char[] readBlock(int offset) throws IOException {
-		char[] block = new char[BLOCK_SIZE];
-		br.read(block, offset, BLOCK_SIZE);
+	public ByteBuffer readBlock(int offset) throws IOException {
+		ByteBuffer block = ByteBuffer.allocate(BLOCK_SIZE);
+		fc.read(block);
+		block.flip();
+		++reads;
 		return block;
+	}
+	
+	/**
+	 * @author Richard
+	 * @throws IOException 
+	 */
+
+	public int getReads() {
+		return reads;
+	}
+	
+	public int getWrites() {
+		return writes;
 	}
 }
