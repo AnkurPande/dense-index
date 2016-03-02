88d110aa7b9efe6949b9b3602eb7389010ff24ce
Richard Kallos <rkallos@gmail.com>
2016-03-02 14:10:25 -0500

    Added main() to CreateIndex, removed index creation from QueryCLI

--- src/indexing/CreateIndex.java
+++ src/indexing/CreateIndex.java
@@ -8,6 +8,7 @@ package indexing;
 import input_output.IOFile;
 import input_output.IndexWriter;
 import input_output.WrongRecordOffsetSizeException;
+import model.Performance;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -20,11 +21,27 @@ public class CreateIndex {
 	static final int RECORD_SIZE = IOFile.RECORD_SIZE;
 	static final int AGE_OFFSET = IOFile.AGE_OFFSET;
 	
-	static String path = "C:\\Users\\Ankurp\\DENSE-INDEX\\";
-	//static String path = "./resources/relation/";
+	//static String path = "C:\\Users\\Ankurp\\DENSE-INDEX\\";
+	static String path = "./resources/relation/";
 	static String FILE_NAME = path+"person.txt";
 	IndexWriter writer ;
 	IOFile f;
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		CreateIndex c = new CreateIndex();
+		Performance perf = new Performance();
+		
+		perf.calculateStartMemory();
+		perf.startTimer();
+		c.runCases();
+		perf.stopTimer();
+		perf.calculateEndMemory();
+		
+		System.out.println("\nPerformance data for index creation.");
+		System.out.println("Time Taken : " + perf.getTimeElapsed() + "ms");
+		System.out.println("Memory Taken (in bytes): " + perf.getMemUsed()+ " bytes");
+		System.out.println("Memory Taken (in MB): " + (double) perf.getMemUsed() /(1024*1024) + " MB");
+	}
 	
 	public CreateIndex() throws FileNotFoundException{
 		f = new IOFile(FILE_NAME);
--- src/lookup/QueryCLI.java
+++ src/lookup/QueryCLI.java
@@ -14,7 +14,6 @@ public class QueryCLI {
 		
 		LookupManager lm = null;
 		CreateIndex ci = null;
-		Performance index_performance = new Performance();
 		Performance lookup_performance = new Performance();
 		Scanner kb = new Scanner(System.in);
 		
@@ -23,36 +22,6 @@ public class QueryCLI {
 		System.out.println("| Authors: Ankur Pandey, Julian Enoch, Richard Kallos |");
 		System.out.println("+-----------------------------------------------------+");
 
-		//Open relation file.
-		try {
-			ci = new CreateIndex();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.out.println("Unable to find relation file. Exiting.");
-			System.exit(1);
-		}
-				
-		System.out.println("Creating Index:");
-		try {
-			//Start timer.
-			index_performance.startTimer();
-			//Start memory.
-			index_performance.calculateStartMemory();
-			// Create index
-			ci.runCases();
-			//Stop Timer.
-			index_performance.stopTimer();
-			//end memory.
-			index_performance.calculateEndMemory();
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.println("Unable to create indexes. Exiting.");
-			System.exit(1);
-		}
-		
-		System.out.println("Index created. You may now enter queries");
-		System.out.println("Enter q to quit.");
-		
 		// Instantiate LookupManager
 		try {
 			lm = new LookupManager();
@@ -78,7 +47,6 @@ public class QueryCLI {
 			try {
 				//Start analyzing for indexing.
 				lookup_performance.startTimer();
-				
 				//Calculate start memory.
 				lookup_performance.calculateStartMemory();
 				//Lookup
@@ -94,10 +62,6 @@ public class QueryCLI {
 				System.exit(1);
 			}
 			// TODO: Print hits.txt to console.
-			System.out.println("\nPerformnace data for index creation operation.");
-			System.out.println("Time Taken : " + index_performance.getTimeElapsed() + "ms");
-			System.out.println("Memory Taken (in bytes): " + index_performance.getMemUsed()+ " bytes");
-			System.out.println("Memory Taken (in MB): " + (double) index_performance.getMemUsed() /(1024*1024) + " MB");
 			
 			System.out.println("\nPerformnace data for lookup operation of age value "+ age);
 			System.out.println("Time Taken : " + lookup_performance.getTimeElapsed() + "ms");
