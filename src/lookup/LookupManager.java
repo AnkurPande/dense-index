adb6c8282628cecc8ef28b43b2d575e8f494e1b4
Julian Enoch <julian.enoch@gmail.com>
2016-03-06 15:51:26 -0500

    Move outputWrites

--- src/lookup/LookupManager.java
+++ src/lookup/LookupManager.java
@@ -3,7 +3,6 @@ package lookup;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -17,47 +16,32 @@ import input_output.IOFile;
 public class LookupManager {
 	public static final String INDEX_PATH = "./resources/index/";
 	public static final String HITS_PATH = "./resources/output/hits.txt";
-	private static int outputWrites = 0;
+	private int outputWrites = 0;
 
-	// private static HashMap<Short, IndexEntry> index = new HashMap<>();
-	RandomAccessFile raf;
 	FileOutputStream fos;
 	FileChannel fc;
 	ByteBuffer hitsBuffer;
 
 	public void openOutput() throws FileNotFoundException {
 		fos = new FileOutputStream(HITS_PATH);
-		// raf = new RandomAccessFile(HITS_PATH, "rw");
 		fc = fos.getChannel();
-
 		hitsBuffer = ByteBuffer.allocate(IOFile.BLOCK_SIZE);
 	}
 
-	// public static void main(String[] args) throws IOException {
-	// // index.put((short) 18, new IndexEntry("18", 2));
-	// (new LookupManager()).lookupHits((short) 18);
-	// }
-
 	public void lookupHits(Short age) throws IOException {
+		openOutput();
 
-		// IndexEntry entry = index.get(age);
-		//
-		// long count = entry.getCount();
-		// System.out.println("Number of hits: " + count);
-		//
-		// String bucketName = entry.getBucketName();
 		String bucketName = age.toString();
 
-		long bucketSize = 0;
 		long indexReads = 0;
 		long relationReads = 0;
 		try {
 			IOFile bucketFile = new IOFile(INDEX_PATH + bucketName);
 			IOFile relationFile = new IOFile(IndexCreator.FILE_NAME);
 
-			bucketSize = bucketFile.length();
+			long bucketSize = bucketFile.length();
 			System.out.println("Number of hits:       " + bucketSize / 4);
-			
+
 			int indexOffset = 0;
 			ByteBuffer indexBlock;
 			ByteBuffer dataBlock = null;
@@ -68,21 +52,13 @@ public class LookupManager {
 			long byteOffset;
 			while (indexOffset < bucketSize) {
 				indexBlock = bucketFile.readSequentialBlock();
-				// System.out.println("Index block position: " +
-				// indexBlock.position());
 				indexOffset += IOFile.BLOCK_SIZE;
 
 				currentBlockStart = 0;
 				blockOffset = 0;
-				openOutput();
 				while (indexBlock.hasRemaining()) {
 					offset = indexBlock.getInt();
-					
 					byteOffset = offset * 100;
-
-					// System.out.println("Index block position: " +
-					// indexBlock.position());
-
 					blockOffset = (int) (byteOffset - currentBlockStart);
 
 					if (currentBlockStart == 0 || blockOffset >= 4000) {
@@ -91,28 +67,17 @@ public class LookupManager {
 						blockOffset = 0;
 					}
 
-					// System.out.println("Data block position: " +
-					// dataBlock.position());
-
 					dataBlock.position(blockOffset);
-					// System.out.println("Data block position: " +
-					// dataBlock.position());
 					dataBlock.get(record);
-					// System.out.println("Data block position: " +
-					// dataBlock.position());
 
 					hitsBuffer.put(record);
 
 					if (hitsBuffer.capacity() - hitsBuffer.position() < 100 || !indexBlock.hasRemaining()) {
 						hitsBuffer.flip();
 						while (hitsBuffer.hasRemaining()) {
-							// System.out.println("Hit file position: " +
-							// fc.position());
-							int write = fc.write(hitsBuffer);
-							outputWrites++;
-							// System.out.println("Hit file position: " +
-							// fc.position());
+							fc.write(hitsBuffer);
 						}
+						outputWrites++;
 						hitsBuffer.clear();
 					}
 				}
@@ -121,30 +86,15 @@ public class LookupManager {
 			indexReads = bucketFile.getReads();
 			relationReads = relationFile.getReads();
 		} catch (FileNotFoundException e) {
+			// Index bucket file doesn't exist. Do nothing
 		} finally {
-			
 			System.out.println("Index block reads:    " + indexReads);
 			System.out.println("Relation block reads: " + relationReads);
 			System.out.println("Output block writes:  " + outputWrites);
 		}
 	}
 
-	// public int computeRelationOffset(long offset) {
-	// int relationOffset = (int) ((offset * 100) / IOFile.BLOCK_SIZE);
-	// short blockOffset = (short) ((offset * 100) % IOFile.BLOCK_SIZE);
-	// if (blockOffset == 40){
-	// relationOffset++;
-	// }
-	// return relationOffset;
-	// }
-	//
-	// public short computeBlockOffset(long offset) {
-	// short blockOffset = (short) ((offset * 100) % IOFile.BLOCK_SIZE);
-	// return blockOffset;
-	// }
-
 	public void closeOutput() throws IOException {
-		// raf.close();
 		fos.close();
 		fc.close();
 	}
